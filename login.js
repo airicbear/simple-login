7c7f4e05a1e5dc9926ae728b85cc5ca11d317a72
Eric Nguyen <33969138+airicbear@users.noreply.github.com>
2018-09-08 23:39:57 -0400

    Update form; Update login(); Update passwords

--- login.js
+++ login.js
@@ -7,14 +7,21 @@ function hash(plaintext) {
 }
 
 function login(username, password) {
+  if (!username) {
+    alert("Please enter a username.");
+    return 1;
+  }
   if (userInfo.hasOwnProperty(username)) {
     if (hash(password) === userInfo[username]) {
       alert("Login success.");
+      return 0;
     } else {
-      alert("Login failed.");
+      alert("Incorrect password.");
+      return 1;
     }
   } else {
     alert("Username doesn't exist.");
+    return 1;
   }
 }
 
@@ -22,6 +29,6 @@ let userInfo = {
   "Octocat": "ab6ee2706e4b17cf8328d84ef558a71ac929f816158c4b8d06dc9059b356bf97", // Octocat
   "Nyancat": "e1adca2aab7f058717d5876928d901cc797406cefd6156cc0c58ac12f9f8391f", // Nyan!Cat
   "Dogecoin": "633d2a475429f17a0cbbc4526827d9f787c59c0559b16dcc09055f097ebd0fca", // ToTheMoon
-  "SecretBoi": "6e08e700f8e12d6bcfaee4fe94759dc66cec13be306226740e79b6bd8613fd20", // Try to crack SecretBoi's password
-  "SecretGal": "fb8ee13cda6b0b4fbe2a50336a0431d9ac0d2cb38564c2096ac2eb0ec65c4924", // Try to crack SecretGal's password
+  "SecretBoi": "03f8c1273e3da99bb315fe7b71c8e45159c826e6d3f42eecd4612078248f73a1", // Try to crack SecretBoi's password
+  "SecretGal": "e8cca808ae5aaa03a7fc3060856e6d4ad776fb1cb8cfe441a9bd240988cde151", // Try to crack SecretGal's password
 }
